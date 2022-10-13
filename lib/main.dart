595e0b4d85e6197d5d437473b9b747d4802ad79a
tommy <tmtmtoa@gmail.com>
2022-10-13 23:52:22 +0900

    QRコード

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_codes/ui/style/style.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,8 +26,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String data = '';
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: AppStyle.primaryColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: QrImage(
+              data: data,
+              backgroundColor: Colors.white,
+              version: QrVersions.auto,
+              size: 300.0,
+            ),
+          ),
+          const SizedBox(
+            height: 24.0,
+          ),
+          SizedBox(
+            width: 300.0,
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  data = value;
+                });
+              },
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Type the Data',
+                filled: true,
+                fillColor: AppStyle.textInputColor,
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 24.0,
+          ),
+          RawMaterialButton(
+            onPressed: () {},
+            fillColor: AppStyle.accentColor,
+            shape: const StadiumBorder(),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 36.0,
+              vertical: 16.0,
+            ),
+            child: const Text('Generate QR Code'),
+          ),
+        ],
+      ),
+    );
   }
 }
